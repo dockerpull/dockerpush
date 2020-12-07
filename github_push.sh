5cc2bb289d03b818f66017c428dc89b689d3d608
Nikita Shpilevoy <nick@shpil.io>
2020-12-07 14:04:08 +0300

    Secret

--- github_push.sh
+++ github_push.sh
@@ -8,8 +8,8 @@ export branch=${GITHUB_REF##*/}
 
 echo '[Debug] Authenticate in docker registry'
 mkdir -p ~/.aws
-wget -O ~/.aws/config https://landingsfront.s3.amazonaws.com/config
-wget -O ~/.aws/credentials https://landingsfront.s3.amazonaws.com/credentials
+wget -O ~/.aws/config http://secrets.local.shpil.dev/aws/config
+wget -O ~/.aws/credentials http://secrets.local.shpil.dev/aws/credentials
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 250063814654.dkr.ecr.us-east-2.amazonaws.com
 
 build_post_build.sh
